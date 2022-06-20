a89db59c1899015e006d481e05c141a6f8b26e3e
NRalston <Neil.Ralston@MSPMAC.ORG>
2022-06-19 19:56:24 -0500

    updating branch

--- Mapping_GeoJSON_Linestrings/Static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/Static/js/logic.js
@@ -16,32 +16,64 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     maxZoom: 18,
     accessToken: API_KEY
 });
+
+// We create the light view tile layer that will be an option for our map.
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
-  Dark: dark
+  Dark: dark,
+  Light: light
 };
 // Create the map object with center and zoom level.
-//let map = L.map('mapid').setView([30, 30], 2);
+//let map = L.map('mapid').setView([44.0, -80.0], 2);
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
   center: [30, 30],
   zoom: 2,
-  layers: [streets]
+  layers: [light]
 })
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/neilralston/Mapping_Earthquakes/main/majorAirports.json";
+//let airportData = "https://raw.githubusercontent.com/neilralston/Mapping_Earthquakes/main/majorAirports.json";
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/neilralston/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-  console.log(data)
-  // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map)
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+  //color: 'yellow',
+  //weight: 2,
+  style: myStyle,
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr> <h2> Destination: " + feature.properties.dst + "</h3>");
+  }
+})
+.addTo(map);
 });
+
+// Grabbing our GeoJSON data.
+//d3.json(airportData).then(function(data) {
+ // console.log(data)
+  // Creating a GeoJSON layer with the retrieved data.
+//L.geoJSON(data).addTo(map)
+//});
 
 
 // Add GeoJSON data.
@@ -84,13 +116,13 @@ L.geoJSON(data).addTo(map)
 
 // Coordinates for each point to be used in the line.
 // Coordinates for each point to be used in the polyline.
-let line = [
-    [37.6213, -122.3790],
-    [30.1975, -97.6664],
-    [44.8848, -93.2223],
-    [43.6777, -79.6248],
-    [40.6413, -73.7781],
-  ];
+//let line = [
+   // [37.6213, -122.3790],
+   // [30.1975, -97.6664],
+    //[44.8848, -93.2223],
+   // [43.6777, -79.6248],
+   // [40.6413, -73.7781],
+ // ];
 
 // Create a polyline using the line coordinates and make the line red.
 //L.polyline(line, {
@@ -117,5 +149,5 @@ let line = [
 //});
 
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+//streets.addTo(map);
 
